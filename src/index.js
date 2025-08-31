40486648c4472d0e4160e5768d220d0091f97653
Lesetja <kmalapane155@gmail.com>
2025-08-31 14:23:14 +0200

    im smart as fuck i wont lie

--- src/index.js
+++ src/index.js
@@ -27,65 +27,40 @@ function fibsRec(n) {
 
 console.log(fibsRec(8));
 
-// function mergeSort(array = []) {
-//   if (array.length === 1) return array;
+function mergeSort(array) {
+  if (array.length === 1) return array;
+  let newArray = [];
 
-//   let array1 = array.slice(0, parseInt(array.length / 2));
-//   let array2 = array.slice(parseInt(array.length / 2), parseInt(array.length));
+  // split array
+  let array1 = mergeSort(array.slice(0, parseInt(array.length / 2)));
+  let array2 = mergeSort(
+    array.slice(parseInt(array.length / 2), parseInt(array.length)),
+  );
 
-//   console.log("first: " + array1);
-//   console.log("Second: " + array2);
+  // merge array and sort algorithm
+  const ansd = array1.length + array2.length;
 
-//   let newSortedArray = [];
-//   while (true) {
-//     let firstIndex = 0;
-//     let secondIndex = 0;
+  while (newArray.length < ansd) {
+    if (array1.length === 0) {
+      newArray = newArray.concat(array2);
+      array2 = [];
+      break;
+    } else if (array2.length === 0) {
+      newArray = newArray.concat(array1);
+      array1 = [];
+      break;
+    }
 
-//     if (mergeSort(array1)[firstIndex] < mergeSort(array2)[secondIndex]) {
-//       newSortedArray.push(mergeSort(array1)[firstIndex]);
-//       firstIndex += 1;
-//     } else {
-//       newSortedArray.push(mergeSort(array2)[secondIndex]);
-//     }
-//   }
-// }
-
-// function mergeSort(array) {
-//   if (array.length === 1) return array;
-
-//   // split array
-//   let array1 = array.slice(0, parseInt(array.length / 2));
-//   let array2 = array.slice(parseInt(array.length / 2), parseInt(array.length));
-
-//   // merge array and sort algorithm
-
-//   // return newArray;
-// }
-
-const array1 = [2, 3, 5, 7];
-const array2 = [1, 4, 9, 11];
-const ansd = array1.length + array2.length;
-
-console.log(ansd);
-
-let array3 = [];
-
-while (array3.length < ansd) {
-  if (array1.length === 0) {
-    array3.concat(array2);
-    break;
-  } else if (array2.length === 0) {
-    array3.concat(array1);
-    break;
+    if (array1[0] <= array2[0]) {
+      newArray.push(array1.shift());
+    } else if (array2[0] <= array1[0]) {
+      newArray.push(array2.shift());
+    }
+    console.log(newArray);
   }
-
-  if (array1[0] < array2[0]) {
-    console.log("Array1: " + array1[0] + "  Array2: " + array2[0]);
-    array3.push(array1.shift());
-  } else if (array2[0] < array1[0]) {
-    console.log("Array1: " + array1[0] + "  Array2: " + array2[0]);
-    array3.push(array2.shift());
-  }
-
-  console.log("Array3: " + array3);
+  return newArray;
 }
+
+// let myArray = [3, 2, 1, 13, 8, 5, 0, 1];
+let myArray = [3, 2];
+console.log(mergeSort(myArray));
